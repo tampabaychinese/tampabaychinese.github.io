0264c66f9523f4086baff96546d3b4ce5d37a933
Yili Liu <akoliu8@gmail.com>
2023-07-19 13:02:07 -0400

    deployment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,13 @@
 import React from "react";
 import "./Home.css";
 
-const styles = {
-  mainImage: {
-    color: "black",
-  },
-};
-
 const Home = () => {
   return (
-    <div style={styles.mainImage}>
+    <div>
       <div className="mainImageContainer">
         <h1>placeholder text here</h1>
         <h4>placeholder subtitle here</h4>
       </div>
-
       <h3>Home</h3>
     </div>
   );
